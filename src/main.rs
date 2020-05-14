9203801b6dc07f3c74c166d9fc01a4e7ae15e3e3
raimon <raimon49@hotmail.com>
2020-05-14 18:33:53 +0900

    Add a code about using HashMap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::collections::HashMap;
+
+type Table = HashMap<String, Vec<String>>;
+
+fn show(table: Table) {
+    for (artist, works) in table {
+        println!("works by {}:", artist);
+        for work in works {
+            println!(" {}", work);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut table = Table::new();
+    table.insert("Gesualdo".to_string(),
+                 vec!["many madrigals".to_string(),
+                      "Tenebrae Responsoria".to_string()]);
+    table.insert("Caravaggio".to_string(),
+                 vec!["The Musitians".to_string(),
+                      "The Calling of St. Matthew".to_string()]);
+    table.insert("Cellini".to_string(),
+                 vec!["Peres with the head of Medusa".to_string(),
+                      "a salt cellar".to_string()]);
+
+    show(table);
 }
