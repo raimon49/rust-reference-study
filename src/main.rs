4c579f2381b440678b620e5f2363698e57fa8f95
raimon <raimon49@hotmail.com>
2020-06-09 17:49:42 +0900

    Use shared reference at call 'show'

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ use std::collections::HashMap;
 
 type Table = HashMap<String, Vec<String>>;
 
-fn show(table: Table) {
+fn show(table: &Table) {
     for (artist, works) in table {
+        // 関数showの仮引数が&Table型の場合、forループ変数artistも&String型となる
         println!("works by {}:", artist);
         for work in works {
+            // 関数showの仮引数が&Table型の場合、forループ変数workも&Vec<String>型となる
             println!(" {}", work);
         }
     }
@@ -23,7 +25,7 @@ fn main() {
                  vec!["Peres with the head of Medusa".to_string(),
                       "a salt cellar".to_string()]);
 
-    show(table);
-    // 「value borrowed here after move」で怒られる
-    // assert_eq!(table["Gesualdo"][0], "many madrigals");
+    show(&table);
+    // 参照渡しをしなかった場合、関数側に所有権が移って「value borrowed here after move」で怒られる
+    assert_eq!(table["Gesualdo"][0], "many madrigals");
 }
