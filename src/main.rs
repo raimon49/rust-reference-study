81e3ee945f1d78b40fca2df0d74af4648e71be8b
raimon <raimon49@hotmail.com>
2020-06-28 09:35:21 +0900

    Define func 'smallest'

--- src/main.rs
+++ src/main.rs
@@ -114,6 +114,15 @@ fn f(p: &'static i32) {
     }
 }
 
+fn smallest(v: &[i32]) -> &i32 {
+    let mut s = &v[0];
+    for r in &v[1..] {
+        if *r < *s { s = r; }
+    }
+
+    s
+}
+
 fn main() {
     let mut table = Table::new();
     table.insert("Gesualdo".to_string(),
